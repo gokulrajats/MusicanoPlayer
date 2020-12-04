80b240de40c433f0eed17e796ae8a4437198e72b
gokulrajats <tsgr1999@gmail.com>
2020-12-04 23:36:50 +0530

    Notification Manager setup

--- app/src/main/java/com/example/musicanos/others/Constants.kt
+++ app/src/main/java/com/example/musicanos/others/Constants.kt
@@ -2,4 +2,7 @@ package com.example.musicanos.others
 
 object Constants {
     const val COLLECTION_NAME = "songs"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
+
 }
